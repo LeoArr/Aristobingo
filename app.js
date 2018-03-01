eba7d760d45407b0718c24abd9cb91091ce29bf3
Leopold Arreström <leopold.arrestrom@gmail.com>
2018-03-01 17:33:41 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+"use strict";
+
+window.onload = load_new;
+
+function setup_popup() {
+    var popup = document.getElementById("lucka_popup");
+    var popupclose = document.getElementById("closeModal");
+    popupclose.onclick = function() {
+        popup.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+    return popup;
+}
+
+function set_popup_content(i) {
+    var cont = document.getElementById("lucka_container");
+    var res ="";
+    switch(i) {
+        case '1':
+            res = "ABSINT";
+            break;
+        case '2':
+            res = "ARISTO-POD";
+            break;
+        case '3':
+            res = "EN FUCK ME UP!"
+            break;
+        default:
+            res = "404";
+    }
+    cont.innerText = res;
+}
+
+function load_new() {
+    var popup = setup_popup();
+    var luckor = document.getElementById('goa_luckor_container');
+    for (var i = 0; i < 25; i++) {
+        var lucka = document.createElement('div')
+        var number = document.createElement('div');
+        lucka.classList.add("lucka_stängd");
+        number.classList.add("lucka_number");
+        number.innerText = i + 1;
+        if ((i % 5) == 0)
+            lucka.style.clear = "left";
+        lucka.appendChild(number);
+        luckor.appendChild(lucka);
+        lucka.onclick = function(event) {
+            popup.style.display = "block";
+            set_popup_content(event.target.innerText);
+        }
+    }
+}
